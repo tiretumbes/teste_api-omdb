ee658f40fa08b3e7f0fe0ad3d78bc196d4fa94ba
Well <tiretumbes4@gmail.com>
2026-08-26 10:24:15 -0300

    projeto commitado

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+
+
+import axios from 'axios'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const[filmes, setFilmes] = useState([]);
+  const[busca, setBusca] = useState("");
+ 
+  const chave = import.meta.env.VITE_API_KEY
+
+  const buscaFilme = async () =>{
+    try{
+        const data = await axios.get("http://www.omdbapi.com/?apikey=" + chave + "&s=" + busca);
+
+        setFilmes(data.data.Search);
+
+    }
+
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      {chave}
+
+      {
+        filmes.map((filme: any) => {
+          return(
+            <div className="filme" key={filme.imdbID}>
+              <h1>{filme.Title}</h1>
+              <h3>{filme.Year}</h3>
+              <img src={filme.Poster} alt={filme.Title} />
+            </div>
+
+            
+          )
+        }
+        )
+      } 
+
+      <div className="busca">
+
+      <input type="text" value={busca} onChange={(valor) => setBusca(valor.target.value)} />
+      <button onClick={buscaFilme}>Buscar Filmes</button>
+        </div>
+    </>
+  )
+}
+
+export default App
