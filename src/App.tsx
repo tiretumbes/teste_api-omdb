acf3e5d65a6a162b81c48545b01f4d9bf9d42222
TIRETUMBES <>
2026-08-28 10:20:54 -0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Detalhes from './pages/Detalhes';
 
 
-import axios from 'axios'
-import './App.css'
-import { useState } from 'react'
-
-function App() {
-  const[filmes, setFilmes] = useState([]);
-  const[busca, setBusca] = useState("");
- 
-  const chave = import.meta.env.VITE_API_KEY
-
-  const buscaFilme = async () =>{
-    try{
-        const data = await axios.get("http://www.omdbapi.com/?apikey=" + chave + "&s=" + busca);
-
-        setFilmes(data.data.Search);
-
-    }
-
-    catch(error){
-      console.log(error)
-    }
-  }
-
+export default function App() {
   return (
-    <>
-      {chave}
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/detalhes">Detalhes</Link></li>
+        </ul>
+      </nav>
 
-      {
-        filmes.map((filme: any) => {
-          return(
-            <div className="filme" key={filme.imdbID}>
-              <h1>{filme.Title}</h1>
-              <h3>{filme.Year}</h3>
-              <img src={filme.Poster} alt={filme.Title} />
-            </div>
-
-            
-          )
-        }
-        )
-      } 
-
-      <div className="busca">
-
-      <input type="text" value={busca} onChange={(valor) => setBusca(valor.target.value)} />
-      <button onClick={buscaFilme}>Buscar Filmes</button>
-        </div>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detalhes" element={<Detalhes />} />
+        
+      </Routes>
+    </Router>);
 }
-
-export default App
---
+++ src/pages/Detalhes.tsx
@@ -0,0 +1,11 @@
+
+
+function Detalhes() {
+  return (
+    <div>
+      <h1>Detalhes do Filme</h1>
+      <p>Aqui você pode exibir informações detalhadas sobre o filme selecionado.</p>
+    </div>
+  );
+}
+export default Detalhes;
